d1d343e0dfe62337297d66975777dd51debaf606
John Lindquist <johnlindquist@gmail.com>
2025-03-20 10:36:22 -0600

    fix: Update test to use Request object

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,7 +3,8 @@ import app from './index'
 
 describe('GET /', () => {
     it('should return "Hello Hono!"', async () => {
-        const response = await app.fetch('http://localhost/')
+        const req = new Request('http://localhost/')
+        const response = await app.fetch(req)
         const text = await response.text()
         expect(text).toBe('Hello Hono!')
     })
